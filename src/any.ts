72fcdaecbbed0197dfbb2e8f82583fcd46b85c6e
yusakuNakano <senpukuzidouketu@gmail.com>
2021-08-25 13:33:52 +0900

    create any.ts

---
+++ src/any.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+export {};
+
+let url: string =
+  'http://udemy-utils.herokuapp.com/api/v1/articles?token=token123';
+
+axios.get(url).then(function (response) {
+  interface Article {
+    id: number;
+    title: string;
+    description: string;
+  }
+
+  let data: Article[];
+  data = response.data;
+  data = [
+    {
+      id: 1,
+      title: 'title',
+      description: 'description',
+    },
+  ];
+  console.log(data);
+});
