5a8474a828cdc4826e4a1e69c64ccc9eead41ce2
yusakuNakano <senpukuzidouketu@gmail.com>
2021-08-26 13:55:30 +0900

    create return-of-interfaces.ts

---
+++ src/return-of-interfaces.ts
@@ -0,0 +1,26 @@
+export {};
+
+class Mahoutsukai {}
+class Souryo {}
+
+class Taro extends Mahoutsukai {}
+
+interface Kenja {
+  ionazun(): void;
+}
+
+interface Senshi {
+  kougeki(): void;
+}
+class Jiro implements Kenja, Senshi {
+  ionazun(): void {
+    console.log('ionazun');
+  }
+  kougeki(): void {
+    console.log('kougeki');
+  }
+}
+
+const jiro = new Jiro();
+jiro.ionazun();
+jiro.kougeki();
