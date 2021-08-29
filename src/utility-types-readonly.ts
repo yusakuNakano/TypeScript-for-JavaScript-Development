450efc73e705b7e71544cac915ccc50a2fea9689
yusakuNakano <senpukuzidouketu@gmail.com>
2021-08-30 08:59:37 +0900

    create utility-types-readonly

---
+++ src/utility-types-readonly.ts
@@ -0,0 +1,28 @@
+export {};
+
+type Profile = {
+  name: string;
+  age: number;
+};
+
+const me: Profile = {
+  name: 'Ham',
+  age: 43,
+};
+
+me.age++;
+console.log(me);
+
+type PersonalDataType = Readonly<Profile>;
+
+const friend: PersonalDataType = {
+  name: 'shigeru',
+  age: 40,
+};
+
+// friend.age++;
+
+type YomitoriSenyo<T> = {
+  readonly [P in keyof T]: T[P];
+};
+type YomitoriSenyoProfile = YomitoriSenyo<Profile>;
