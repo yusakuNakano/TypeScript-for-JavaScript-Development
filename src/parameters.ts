7805ba3f0c6103776d5f94889988382259a1be7b
yusakuNakano <senpukuzidouketu@gmail.com>
2021-08-30 10:49:22 +0900

    create Parameters.ts

---
+++ src/parameters.ts
@@ -0,0 +1,18 @@
+export {};
+const debugProfile = (name: string, age: number) => {
+  console.log({ name, age });
+};
+
+debugProfile('Ham', 43);
+
+type Profile = Parameters<typeof debugProfile>;
+
+const profile: Profile = ['Gloroa', 76];
+
+debugProfile(...profile);
+
+type MyParameters<T extends (...args: any) => any> = T extends (
+  ...args: infer P
+) => any
+  ? P
+  : never;
