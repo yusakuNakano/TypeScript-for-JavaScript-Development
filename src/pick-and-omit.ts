da5bd637ad0520e2cd36697e920c9db2bfabea2e
yusakuNakano <senpukuzidouketu@gmail.com>
2021-08-30 10:10:50 +0900

    create pick-omit.ts

---
+++ src/pick-and-omit.ts
@@ -0,0 +1,10 @@
+export {};
+
+type DetailedProfile = {
+  name: string;
+  height: number;
+  weight: number;
+};
+
+type SimpleProfile = Pick<DetailedProfile, 'name' | 'weight'>;
+type SmallProfile = Omit<DetailedProfile, 'height'>;
